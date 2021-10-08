3a044677b3c3ded12926e453cd7a8d956a0040ac
Cyril Pierron <cype59@gmail.com>
2021-10-08 16:08:18 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4effde33deb68128c43c8812ad1119de'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b9023cc0-1b41-42c7-82d5-2c262179b1f6'
+};
