0b45a292155f34c95d6edf4fb00922397df147de
Cyril Pierron <cype59@gmail.com>
2021-10-11 16:59:58 +0200

    update cloudinary

--- config/plugin.js
+++ config/plugin.js
@@ -1,16 +1,10 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
-    provider: 'cloudinary',
+    provider: "cloudinary",
     providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
-    },
-    actionOptions: {
-      upload: {},
-      delete: {},
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_API_KEY"),
+      api_secret: env("CLOUDINARY_API_SECRET"),
     },
   },
-  // ...
 });
